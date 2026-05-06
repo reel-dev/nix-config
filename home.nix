6a95a2e47c03101910543194a66768cde064b6fc
Rene Elbracht <elbrachtrene@gmail.com>
2026-05-06 21:36:50 +0200

    (-) nano. (+) helix. (configure) starship formatting, git, zsh.

--- home.nix
+++ home.nix
@@ -5,9 +5,7 @@
   home.homeDirectory = "/home/reel";
   home.stateVersion = "25.11";
   home.packages = [
-    pkgs.nano
     pkgs.openssh
-    pkgs.git
   ];
 
   home.file = {
@@ -16,18 +14,53 @@
   home.sessionVariables = {
   };
 
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "René Elbracht";
+        email = "elbrachtrene@gmail.com";
+      };
+    };
+  };  
   programs.zsh = {
     enable = true;
+    initContent = ''
+      export TERM=xterm-256color
+    '';
   };
   programs.starship = {
     enable = true;
     settings = {
       add_newline = true;
+      
+      format = "$username$directory$character";
+      right_format = "$git_branch$git_status";      
+
       character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol = "[➜](bold red)";
+        success_symbol = "[➜](#9ece6a)";
+        error_symbol = "[➜](#f7768e)";
+      };
+      username = {
+        show_always = true;
+        format = "[$user](bold #7aa2f7)@";
+      };
+      directory = {
+        format = "[$path](bold #e0af68) ";
+        truncation_length = 0;
+        truncate_to_repo = false;
+      };
+      git_branch = {
+        format = "[$symbol$branch](bold #9ece6a)";
+        symbol = " ";
+      };
+      git_status = {
+        format = "[$all_status$ahead_behind](#f7768e)";
       };
     };
+  };
+  programs.helix = {
+    enable = true;
   };
 
   programs.home-manager.enable = true;
