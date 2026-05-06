3a7bae47cbaec10708ccfca5292ed4065cf756dd
Rene Elbracht <elbrachtrene@gmail.com>
2026-05-06 22:04:48 +0200

    remove comments

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Home Manager configuration of reel";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -20,12 +19,7 @@
       homeConfigurations."reel" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
